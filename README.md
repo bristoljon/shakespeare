8026ef95b55c4a068de2f84b0e9d0a9ed8842da3
Jon Wyatt <jon@bristoljon.uk>
2016-05-07 14:47:52 +0100

    Initial commit - basic functionality

---
+++ README.md
@@ -0,0 +1,6 @@
+Shakespearean Insult Generator
+====
+
+Quick and easy Shakespearean insult generator based off of this list..
+
+---
---
+++ src/app.jsx
@@ -0,0 +1,35 @@
+var React = require('react');
+
+function makeInsult () {
+  var col = [["artless", "bawdy", "beslubbering", "bootless", "churlish", "cockered", "clouted", "craven", "currish", "dankish", "dissembling", "droning", "errant", "fawning", "fobbing", "froward", "frothy", "gleeking", "goatish", "gorbellied", "impertinent", "infectious", "jarring", "loggerhead", "lumpish", "mammering", "mangled", "mewling", "paunchy", "pribbling", "puking", "puny", "qualling", "rank", "reeky", "roguish", "ruttish", "saucy", "spleeny", "spongy", "surly", "tottering", "unmuzzled", "vain", "venomed", "villainous", "warped", "wayward", "weedy", "yeasty"],
+              ["base-court", "bat-fowling", "beef-witted", "beetle-headed", "boil-brained", "clapper-clawed", "clay-brained", "common-kissing", "crook-pated", "dismal-dreaming", "dizzy-eyed", "doghearted", "dread-bolted", "earth-vexing", "elf-skinned", "fat-kidneyed", "fen-sucked", "flap-mouthed", "fly-bitten", "folly-fallen", "fool-born", "full-gorged", "guts-griping", "half-faced", "hasty-witted", "", "hedge-born", "hell-hated", "idle-headed", "ill-breeding", "ill-nurtured", "knotty-pated", "milk-livered", "motley-minded", "onion-eyed", "plume-plucked", "pottle-deep", "pox-marked", "reeling-ripe", "rough-hewn", "rude-growing", "rump-fed", "shard-borne", "sheep-biting", "spur-galled", "swag-bellied", "tardy-gaited", "tickle-brained", "toad-spotted", "unchin-snouted", "weather-bitten"],
+              ["apple-john", "baggage", "barnacle", "bladder", "boar-pig", "bugbear", "bum-bailey", "canker-blossom", "clack-dish", "clotpole", "coxcomb", "codpiece", "death-token", "dewberry", "flap-dragon", "flax-wench", "flirt-gill", "foot-licker", "fustilarian", "giglet", "gudgeon", "haggard", "harpy", "hedge-pig", "horn-beast", "hugger-mugger", "joithead", "lewdster", "lout", "maggot-pie", "malt-worm", "mammet", "measle", "minnow", "miscreant", "moldwarp", "mumble-news", "nut-hook", "pigeon-egg", "pignut", "puttock", "pumpion", "ratsbane", "scut", "skainsmate", "strumpet", "varlot", "vassal", "whey-face", "wagtail"]];
+  function rand (ar) {
+    return ar[Math.floor(ar.length * Math.random())]
+  };
+  return 'Thou ' + rand(col[0]) + ' ' + rand(col[1]) + ' ' + rand(col[2]);
+};
+
+var App = React.createClass({
+  getInitialState: function () {
+    return {
+        insult: [makeInsult()]
+    };
+  },
+  handleClick: function () {
+    this.setState({ insult: [makeInsult(), ...this.state.insult] });
+  },
+  render: function () {
+    let list = this.state.insult.map(function (insult) {
+      return <h3>{insult}</h3>
+    });
+    return <div className="app">
+      <button onClick={this.handleClick} className="btn btn-success">
+        Again
+      </button>
+      {list}
+    </div>
+  }
+})
+
+React.render(React.createElement(App,{}), document.querySelector('.container'));
